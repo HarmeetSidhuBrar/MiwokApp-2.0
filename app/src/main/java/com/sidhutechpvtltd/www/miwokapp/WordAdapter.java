27dc7212c1a0cdd9ade4dea2bd02bd4bd391ebbc
HarmeetSidhuBrar <35139559+HarmeetSidhuBrar@users.noreply.github.com>
2018-11-03 12:15:08 +0530

    Miwok app 2.0
    Fragments are added

---
+++ app/src/main/java/com/sidhutechpvtltd/www/miwokapp/WordAdapter.java
@@ -0,0 +1,90 @@
+package com.sidhutechpvtltd.www.miwokapp;
+
+import android.app.Activity;
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.view.menu.ListMenuItemView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * {@link WordAdapter} is an {@link ArrayAdapter} that can provide the layout for each list item
+ * based on a data source, which is a list of {@link Word} objects.
+ */
+public class WordAdapter extends ArrayAdapter<Word>  {
+
+    /** Resource id for background color. */
+    private int mColorResourceId ;
+
+    /**
+     * Create a new {@link WordAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.
+     */
+    public WordAdapter(Context context, ArrayList<Word> words,int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId ;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
+        TextView miwokTextView =  listItemView.findViewById(R.id.miwok_text_view);
+        // Get the Miwok translation from the currentWord object and set this text on
+        // the Miwok TextView.
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID default_text_view.
+        TextView defaultTextView =  listItemView.findViewById(R.id.default_text_view);
+        // Get the default translation from the currentWord object and set this text on
+        // the default TextView.
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        //Find the ImageView in the list_item_xml
+        ImageView imageResourceId = listItemView.findViewById(R.id.image);
+        // if image is provided then image view is going to show in activity.
+        if (currentWord.hasImage()) {
+        // Set the ImageView to the image resource specified in the current word.
+            imageResourceId.setImageResource(currentWord.getmImageResourceId());
+        // Make sure that image is going to displayed.
+            imageResourceId.setVisibility(View.VISIBLE);
+        }
+
+        else {
+        // Otherwise it is going to hide the image.
+            imageResourceId.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
+}
