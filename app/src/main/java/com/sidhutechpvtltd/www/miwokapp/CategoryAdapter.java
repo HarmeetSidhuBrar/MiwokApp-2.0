98a2b8b155ea153114849fab16db96f1e7f019da
HarmeetSidhuBrar <35139559+HarmeetSidhuBrar@users.noreply.github.com>
2018-11-06 16:50:28 +0530

    Cpmlete code

--- app/src/main/java/com/sidhutechpvtltd/www/miwokapp/CategoryAdapter.java
+++ app/src/main/java/com/sidhutechpvtltd/www/miwokapp/CategoryAdapter.java
@@ -1,5 +1,7 @@
 package com.sidhutechpvtltd.www.miwokapp;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,14 +11,20 @@ import android.support.v4.app.FragmentPagerAdapter;
  * each list item based on a data source which is a list of  objects.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private Context mContext;
+
     /**
      * Create a new {@link CategoryAdapter} object.
      *
+     * @param context is the context of the app
      * @param fm is the fragment manager that will keep each fragment's state in the adapter
      *           across swipes.
      */
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(Context context,FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
     /**
      * Return the {@link Fragment} that should be displayed for the given page number.
@@ -39,5 +47,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
